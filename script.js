b9c7d11cd258b62920011a3064eecc37e17a15a9
mattschneble <mattschneble@gmail.com>
2023-06-30 17:35:55 -0700

    added variables and values for password generator

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//created variables for the password generator
+var passwordLength;
+var chooseNumbers;
+var chooseSpecialCharacters;
+var chooseUppercaseLetters;
+var chooseLowercaseLetters;
+
+//entered special characters, numbers, uppercase and lowercase letters to be used by the password generator
+specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "~", "`", ":", ";", "<", ">", "?", "/", "{", "}", "[", "]", "|", "]", "'", ",", "."];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//for selection(s) made outside the if statement, creating a variable to hold these for if they are needed later
+var selection;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
